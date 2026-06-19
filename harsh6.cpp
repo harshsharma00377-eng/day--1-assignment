693637423e198a47e6ecfac883724fe268454d63
Harsh sharma <harshsharma00377@gmail.com>
2026-06-19 22:55:55 +0530

    Add files via upload

---
+++ harsh6.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float celsius;
+    cin>>celsius;
+    float fahrenheit = (celsius*9.0/5.0)+32;
+    cout<<fahrenheit<<endl;
+    return 0;
+
+}
